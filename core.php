86403fadf2c6f3014b60dc49975e1e12ecdb4d83
alf <sites@hubmed.org>
2010-01-02 14:58:47 +0000

    handle styles

--- core.php
+++ core.php
@@ -4,28 +4,35 @@ function print_html($text){
   print filter_var($text, FILTER_SANITIZE_SPECIAL_CHARS);
 }
 
-if (!is_array($scripts))
+if (is_string($scripts))
   $scripts = array($scripts);
+
+$scripts[] = '../../core.js';
+$scripts[] = 'map.js';
+  
+if (is_string($styles))
+  $styles = array($styles);
+
+$styles[] = '../../style.css';
 ?>
 
 <html>
 <head>
   <meta http-equiv="content-type" value="text/html;charset=utf-8">
   <title><?php print_html($title); ?></title>
-  <link rel="stylesheet" href="../../style.css">
+    
+  <?php foreach ($styles as $style): if (!empty($style)): ?>
+  <link rel="stylesheet" href="<?php print $style; ?>"/>
+  <?php endif; endforeach; ?>
 </head>
 
 <body>
   <!-- Bing needs inline style attribute on the map div -->
   <div id="map" style="height: 100%; width: 100%"></div>
-
-  <!--<script src="../jquery.js"></script>-->
   
-  <?php foreach ($scripts as $script): ?>
+  <?php foreach ($scripts as $script): if (!empty($script)): ?>
   <script src="<?php print $script; ?>"></script>
-  <?php endforeach; ?>
-  
-  <script src="../../core.js"></script>
-  <script src="map.js"></script>
+  <?php endif; endforeach; ?>
+
 </body>
 </html>

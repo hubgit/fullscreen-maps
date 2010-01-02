74cf8d43e8944875814a42768ec1203c1f7bea21
alf <sites@hubmed.org>
2010-01-02 11:30:16 +0000

    added cloudmade

---
+++ cloudmade/bikeways/map.js
@@ -0,0 +1,3 @@
+var cloudmade = new CM.Tiles.CloudMade.Web({ key: "b553a588affe448f80266392d610d1f0", styleId: 1924 });
+map = new CM.Map(mapContainerId, cloudmade);
+map.setCenter(new CM.LatLng(lat, lon), zoom);
